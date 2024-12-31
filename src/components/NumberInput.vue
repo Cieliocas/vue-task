fd1d3281cd4b07c414c93d79eadd43e12d88aa12
Cieliocas <francielioevangelista@hotmail.com>
2024-12-31 03:30:26 -0300

    Implement two-way data binding for NumberInput component

--- src/components/NumberInput.vue
+++ src/components/NumberInput.vue
@@ -6,7 +6,24 @@
 </template>
 
 <script>
+import { ref, watch } from 'vue';
+
+const props = defineProps(['modelValue', 'label']);
+const emit = defineEmits(['update:modelValue']);
+
+const inputValue = ref(props.modelValue);
+
+watch(() => props.modelValue, (newValue) => {
+    inputValue.value = newValue;
+});
+
+const updateValue = () => {
+    emit('update:modelValue', inputValue.value);
+};
+</script>
+
+<style scoped>
 .number-input {
     margin-bottom: 15px;
 }
-</script>
+</style>
